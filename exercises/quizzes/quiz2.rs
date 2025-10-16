896b6e2b2f20703e55d502e32441ebe104d2e4ae
Andrey Khoronko <mail@akhoronko.me>
2025-10-16 18:07:58 +0300

    10-12 chapters solutions

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,23 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = vec![];
+        for (str, cmd) in input {
+            match cmd {
+                Command::Uppercase => output.push(str.to_uppercase()),
+                Command::Trim => output.push(str.trim().to_string()),
+                Command::Append(n) => {
+                    let mut s = str;
+                    for _ in 0..n {
+                        s += "bar";
+                    }
+                    output.push(s);
+                }
+            }
+        }
+        output
+    }
 }
 
 fn main() {
@@ -37,7 +53,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer as transformer;
     use super::Command;
 
     #[test]
